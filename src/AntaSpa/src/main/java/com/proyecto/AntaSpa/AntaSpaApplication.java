d08dea485cf7430a9dabe542cdcd20dc60117e60
Miguel Alejandro <U23200683@utp.edu.pe>
2026-04-28 00:35:03 -0500

    Primer commit: creación de la estructura base del proyecto y archivos iniciales

---
+++ src/AntaSpa/src/main/java/com/proyecto/AntaSpa/AntaSpaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.AntaSpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntaSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntaSpaApplication.class, args);
+	}
+
+}
